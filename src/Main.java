f4ef95398688d608034eabba0e0d882216627629
archit05-dev <archit3775@gmail.com>
2026-06-10 12:55:44 +0530

    Completed Book Class Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import model.Book;
+
+
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Starting point of our application
         System.out.println("Library Management System");
+        // Create some books
+        Book b1= new Book(101 , "Merchant Of Venice" , "William Shankespeare");
+
+        System.out.println(b1);
 
     }
 }
--- src/model/Book.java
+++ src/model/Book.java
@@ -11,12 +11,54 @@ private String title;
 private String author;
 private boolean isIssued;
 
-public Book(int bookId , String title , String author)
+    public Book(int bookId , String title , String author)
           {
             this.bookId=bookId;
             this.title=title;
             this.author=author;
             this.isIssued=false;
           }
+           
+
+     public String getTitle()
+           {
+             return title;
+           }
+      public void setTitle(String title)
+           {
+             this.title=title;
+           }
+
+      public String getAuthor()
+           {
+             return author;
+           }
+      public void setAuthor(String author)
+           {
+             this.author=author;
+           }     
+
+      public boolean isIssued()
+           {
+             return isIssued;
+           }
+           
+      public int getBookId()
+           {
+            return bookId;
+           }
+      
+           
+      @Override
+      public String toString()
+           {
+            return "Book ID: " + bookId + 
+                   "\nTitle: " + title + 
+                   "\nAuthor: " + author + 
+                   "\nAvailable: " + (isIssued ? "No" : "Yes");             
+           }     
+           
+
+
 
 }
